eff3024592d1f9ea319cf00f1df562cac3fda46e
Osamu Koizumi <Osamu.Koizumi@arm.com>
2019-02-15 12:51:38 +0900

    See raw json.

--- pull-notification.sh
+++ pull-notification.sh
@@ -1,13 +1,13 @@
 #!/bin/tcsh 
 
-set apiKey="<< YOUR API KEY >>"
-set deviceId="<< YOUR DEVICE ID >>"   # Endpoint Name
-set resourcePath="3200/0/5501"
+set apiKey=""
+set deviceId=""   # Endpoint Name
+set resourcePath="3200/0/4014"
 
 
 while(1)
   # Show current time
-  echo -n `date +"%I:%M:%S"` 
+  echo `date +"%I:%M:%S"` 
 
   # Read from a resource (asynchronous) 
   set responseCode=`\
@@ -18,28 +18,28 @@ while(1)
 
   if(${responseCode} == 202) then
     # 202 Accepted. Returns an asynchronous response ID. 
-    set value=`\
-      curl -s -X GET \
+    curl -s -X GET \
         https://api.us-east-1.mbedcloud.com/v2/notification/pull \
         -H "authorization: Bearer ${apiKey}" \
-        | python -m json.tool \
-        | grep 'payload' \
-        | awk -F':' '{print $2}' \
-        | sed 's/\}//g' \
-        | sed 's/\"//g' \
-        | base64 --decode`
-    echo " btnCnt=${value}"
+#        | python -m json.tool \
+        > `date "+%Y%m%d-%H%M%S"`.json
+#        | grep 'payload' \
+#        | awk -F':' '{print $2}' \
+#        | sed 's/\}//g' \
+#        | sed 's/\"//g' \
+#        | base64 --decode \
+#        > `date "+%Y%m%d-%H%M%S"`.jpg     
 
   else if(${responseCode} == 200) then
     # 200 Resource value found in cache. Returns the string value of the resource.  
     set cachedValue=`cat body.tmp` 
-    echo " btnCnt=${cachedValue}  (cached)"
+    echo "${cachedValue}"
 
   else
     echo "${responseCode} Unexpected."
 
   endif
 
-  sleep 5
+  sleep 10
 end
 
