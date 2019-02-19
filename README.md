5fe88c561e56490a1b9a57237ccafb7555947e21
Osamu Koizumi <Osamu.Koizumi@arm.com>
2019-02-19 11:57:53 +0900

    Updated for GR-LYCHEE project.

--- README.md
+++ README.md
@@ -4,34 +4,20 @@ Pull notification test script.
 # Prerequisite
 
 * Pelion Account
-* A device which runs firmware provided by [Quick Start](https://cloud.mbed.com/quick-start). Get device id (endpoint name).
+* GR-LYCHEE which runs https://os.mbed.com/users/coisme/code/Pelion-GR-LYCHEE-test/. Get device id (endpoint name).
 * tcsh
-
-Replace the value of `apiKey` and `deviceId` in `pull-notification.sh`.
 
 # Run
 
 Run the script on your terminal.
 
+```
+pull-notification [API_KEY] [DEVICE_ID] [RESOURCE_PATH]
+```
+
+```
+$ ./pull-notification.sh ak_1MDE1YT... 0169036ff89e000000... 3200/0/4014
+```
+
 ### Sample Output
 
-Sample output. The button on the board was pushed twice between 09:43:22 and 09:43:29.
-
-```
-$ ./pull-notification.sh
-09:42:58 btnCnt=7
-09:43:07 btnCnt=7
-09:43:14 btnCnt=7
-09:43:22 btnCnt=7
-09:43:29 btnCnt=8  (cached)
-09:43:35 btnCnt=8  (cached)
-09:43:42 btnCnt=8  (cached)
-09:43:48 btnCnt=8  (cached)
-09:43:55 btnCnt=8  (cached)
-09:44:01 btnCnt=8  (cached)
-09:44:09 btnCnt=8  (cached)
-09:44:15 btnCnt=8  (cached)
-09:44:21 btnCnt=8  (cached)
-09:44:28 btnCnt=8
-09:44:35^C
-```
--- pull-notification.sh
+++ pull-notification.sh
@@ -1,45 +1,46 @@
 #!/bin/tcsh 
 
-set apiKey=""
-set deviceId=""   # Endpoint Name
-set resourcePath="3200/0/4014"
+# Endpoint URI
+set uri="https://api.us-east-1.mbedcloud.com"
+
+if($#argv < 3) then
+  echo "$0 [API_KEY] [DEVICE_ID] [RESOURCE_PATH]"
+  exit
+endif
+
+set apiKey=$argv[1]
+set deviceId=$argv[2]
+set resourcePath=$argv[3]
+
+# Read from a resource (asynchronous) 
+set responseCode=`\
+  curl -s -X GET -w "%{http_code}\n"\
+      -o body.tmp \
+    "${uri}/v2/endpoints/${deviceId}/${resourcePath}" \
+    -H "authorization: Bearer ${apiKey}"` 
+
+if(${responseCode} == 202) then
+  # 202 Accepted. Returns an asynchronous response ID. 
+  curl -s -X GET \
+      "${uri}/v2/notification/pull" \
+      -H "authorization: Bearer ${apiKey}" \
+      | python -m json.tool \
+      | grep 'payload' \
+      | awk -F':' '{print $2}' \
+      | sed 's/\}//g' \
+      | sed 's/\"//g' \
+      | base64 --decode \
+      > `date "+%Y%m%d-%H%M%S"`.jpg 
+#      > `date "+%Y%m%d-%H%M%S"`.json
 
 
-while(1)
-  # Show current time
-  echo `date +"%I:%M:%S"` 
+else if(${responseCode} == 200) then
+  # 200 Resource value found in cache. Returns the string value of the resource.  
+  cat body.tmp  
+else
+  echo "${responseCode} Unexpected."
+endif
 
-  # Read from a resource (asynchronous) 
-  set responseCode=`\
-    curl -s -X GET -w "%{http_code}\n"\
-       -o body.tmp \
-      "https://api.us-east-1.mbedcloud.com/v2/endpoints/${deviceId}/${resourcePath}" \
-      -H "authorization: Bearer ${apiKey}"` 
-
-  if(${responseCode} == 202) then
-    # 202 Accepted. Returns an asynchronous response ID. 
-    curl -s -X GET \
-        https://api.us-east-1.mbedcloud.com/v2/notification/pull \
-        -H "authorization: Bearer ${apiKey}" \
-#        | python -m json.tool \
-        > `date "+%Y%m%d-%H%M%S"`.json
-#        | grep 'payload' \
-#        | awk -F':' '{print $2}' \
-#        | sed 's/\}//g' \
-#        | sed 's/\"//g' \
-#        | base64 --decode \
-#        > `date "+%Y%m%d-%H%M%S"`.jpg     
-
-  else if(${responseCode} == 200) then
-    # 200 Resource value found in cache. Returns the string value of the resource.  
-    set cachedValue=`cat body.tmp` 
-    echo "${cachedValue}"
-
-  else
-    echo "${responseCode} Unexpected."
-
-  endif
-
-  sleep 10
-end
+# clean up
+rm body.tmp
 
